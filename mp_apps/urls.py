fc25077ebc8858cea1a8541686ee0fbd22f512d2
tluolamo <tluolamo@gmail.com>
2020-07-06 13:16:13 -0700

    save viewset version of fileupload

--- mp_apps/urls.py
+++ mp_apps/urls.py
@@ -20,7 +20,7 @@ from mp_apps.mp_api import views
 
 router = routers.DefaultRouter()
 router.register(r"members", views.MemberViewSet)
-# router.register(r'upload', views.FileUploadViewSet)
+# router.register(r"upload", views.FileUploadViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
